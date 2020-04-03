56fd46b71697c84b3af1a50b01535dc2d2409cb2
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-04-03 20:18:55 +0900

    Create utas_auto_extend_session.user.js

---
+++ utas_auto_extend_session.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Auto-extend UTAS Session
+// @namespace    https://github.com/pizzacat83
+// @version      0.1.0
+// @description  UTASセッション自動延長
+// @author       pizzacat83
+// @include      /^https:\/\/utas\.adm\.u-tokyo\.ac\.jp\/campusweb\/campusportal\.do\?page=main/
+// @grant none
+// @updateUrl    https://raw.githubusercontent.com/pizzacat83/userscripts/master/utas_auto_extend_session.user.js
+// @downloadUrl  https://raw.githubusercontent.com/pizzacat83/userscripts/master/utas_auto_extend_session.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(() => {
+        window.extendSession();
+        console.info('extended');
+    }, 5*60*1000);
+})();
