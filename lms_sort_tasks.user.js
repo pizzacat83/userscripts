e7eb307d415e457f1cbf495535b0eddea1b99cbb
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-10-21 23:39:11 +0900

    lms_sort_tasks: add table header

--- lms_sort_tasks.user.js
+++ lms_sort_tasks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sort ITC-LMS Tasks by Due Date
 // @namespace    pizzacat83.com
-// @version      0.1.0
+// @version      0.2.0
 // @description  try to take over the world!
 // @author       pizzacat83
 // @match        https://itc-lms.ecc.u-tokyo.ac.jp/lms/task
@@ -11,12 +11,13 @@
 
 (function() {
     'use strict';
-    $('#onlineCourseForm > div.block.clearfix > div.contents-list > div > div').html(
-        $('.result_list_line').sort(
+    $('#onlineCourseForm > div.block.clearfix > div.contents-list > div > div').html([
+        ...$('.result_list_tag'),
+        ...$('.result_list_line').sort(
             (...els) => {
                 const [x,y] = els.map((e) => new Date($(e).find('.tasklist-mobile-width-deadline').text()));
                 return x > y ? 1 : x < y ? -1 : 0;
             }
-        )
-    );
+        ),
+    ]);
 })();
